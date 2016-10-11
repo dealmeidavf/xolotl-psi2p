9ccb00c5544cdfc7f12a09a496b640217b10ddc2
rothpc <rothpc@ornl.gov>
2016-10-11 18:06:14 +0000

    Making a branch for optimization of xenon_branch.

--- xolotl/xolotlCore/reactants/psiclusters/VCluster.cpp
+++ xolotl/xolotlCore/reactants/psiclusters/VCluster.cpp
@@ -7,31 +7,28 @@
 using namespace xolotlCore;
 
 VCluster::VCluster(int nV, std::shared_ptr<xolotlPerf::IHandlerRegistry> registry) :
-		PSICluster(nV, registry) {
+		PSICluster(registry) {
+	// Set the size
+	size = nV;
 	// Set the reactant name appropriately
 	std::stringstream nameStream;
 	nameStream << "V_" << size;
 	name = nameStream.str();
 	// Set the typename appropriately
-	typeName = "V";
+	typeName = vType;
 
 	// Update the composition map
-	compositionMap["V"] = size;
+	compositionMap[vType] = size;
 
 	// Compute the reaction radius
 	// It is the same formula for HeV clusters
-	reactionRadius = (sqrt(3.0) / 4.0) * xolotlCore::latticeConstant
+	reactionRadius = (sqrt(3.0) / 4.0) * xolotlCore::tungstenLatticeConstant
 			+ pow(
-					(3.0 * pow(xolotlCore::latticeConstant, 3.0) * size)
+					(3.0 * pow(xolotlCore::tungstenLatticeConstant, 3.0) * size)
 							/ (8.0 * xolotlCore::pi), (1.0 / 3.0))
 			- pow(
-					(3.0 * pow(xolotlCore::latticeConstant, 3.0))
+					(3.0 * pow(xolotlCore::tungstenLatticeConstant, 3.0))
 							/ (8.0 * xolotlCore::pi), (1.0 / 3.0));
-}
-
-std::shared_ptr<Reactant> VCluster::clone() {
-	std::shared_ptr<Reactant> reactant(new VCluster(*this));
-	return reactant;
 }
 
 void VCluster::createReactionConnectivity() {
